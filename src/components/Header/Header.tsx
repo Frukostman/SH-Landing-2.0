6e35c4de81b9480d0ff3edd7389d2720ccba5718
Santiago Homps <santiago.homps@netcentric.biz>
2022-11-07 10:56:44 +0100

    avatar routed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,14 @@
 import './header.scss'
 
-import image from '../../../public/images/avatar.jpeg'
-
+// import image from '../../../public/images/avatar.jpeg'
 
 export const Header = () => {
   return (
     <header className="intro">
         <div className="header">
             {/* <img src={`${process.env.PUBLIC_URL}src/assets/images/avatar.jpeg`} alt="avatar" className="img-fluid image"/> */}
-            <img src={image} alt="avatar" className="header__avatar img-fluid image"/>
+            <img src="./images/avatar.jpeg" alt="avatar" className="header__avatar img-fluid image"/>
+            {/* <img src={image} alt="avatar" className="header__avatar img-fluid image"/> */}
             <div className="header__info">
                 <h1 className="header__info title">SANTIAGO <br></br><strong>HOMPS</strong></h1>
                 <p className="header__info data">Front-end developer</p>
